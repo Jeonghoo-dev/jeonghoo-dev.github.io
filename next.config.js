3e78f70ab9b8e4cee94fee49b39b4fa4c8cf2050
jeonghoo <55tggcc@gmail.com>
2024-02-01 21:39:01 +0900

    컨피그와 패키지json

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
 const isProd = process.env.NODE_ENV === 'production';
+const debug = process.env.NODE_ENV !== 'production';
+const repository = 'jeonghoo.github.io';
+
+export const prefix =
+  process.env.NODE_ENV === 'production'
+    ? 'https://jeonghoo.site'
+    : '';
 
 const nextConfig = {
-  assetPrefix: isProd ? 'https://vvenommm.github.io/jeonghoo.github.io/' : '',
+  assetPrefix: isProd ? 'https://jeonghoo.site' : '',
+  // assetPrefix: !debug ? `/${repository}/` : "",
   reactStrictMode: true,
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
