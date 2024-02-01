c71fa52b87a24005d85b2c83166ebc64d5f78446
jeonghoo <55tggcc@gmail.com>
2024-02-01 21:26:55 +0900

    컨피그 수정

--- next.config.js
+++ next.config.js
@@ -1,28 +1,8 @@
-// This file is not going through babel transformation.
-// So, we write it in vanilla JS
-// (But you could use ES2015 features supported by your Node.js version)
-const withCSS = require('@zeit/next-css');
-const debug = process.env.NODE_ENV !== 'production';
+const isProd = process.env.NODE_ENV === 'production';
 
-module.exports = withCSS({
-  exportPathMap: function () {
-    return {
-      '/': { page: '/' },
-      '/resume': { page: '/resume' },
-    };
-  },
-  assetPrefix: !debug ? '' : '',
-  webpack: (config, { dev }) => {
-    // Perform customizations to webpack config
-    // console.log('webpack');
-    // console.log(config.module.rules, dev);
-    config.module.rules = config.module.rules.map((rule) => {
-      if (rule.loader === 'babel-loader') {
-        rule.options.cacheDirectory = false;
-      }
-      return rule;
-    });
-    // Important: return the modified config
-    return config;
-  },
-});
+const nextConfig = {
+  assetPrefix: isProd ? 'https://vvenommm.github.io/jeonghoo.github.io/' : '',
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
