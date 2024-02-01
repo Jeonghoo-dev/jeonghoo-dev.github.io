b65f00d245ac3208d2968fd52bbf6d25e101e90b
jeonghoo <55tggcc@gmail.com>
2024-02-01 21:48:01 +0900

    컨피그에 output:export 넣고 스크립트에서 next export 삭제

--- next.config.js
+++ next.config.js
@@ -2,12 +2,13 @@ const isProd = process.env.NODE_ENV === 'production';
 const debug = process.env.NODE_ENV !== 'production';
 const repository = 'jeonghoo.github.io';
 
-export const prefix =
-  process.env.NODE_ENV === 'production'
-    ? 'https://jeonghoo.site'
-    : '';
+// export const prefix =
+//   process.env.NODE_ENV === 'production'
+//     ? 'https://jeonghoo.site'
+//     : '';
 
 const nextConfig = {
+  output: 'export',
   assetPrefix: isProd ? 'https://jeonghoo.site' : '',
   // assetPrefix: !debug ? `/${repository}/` : "",
   reactStrictMode: true,
